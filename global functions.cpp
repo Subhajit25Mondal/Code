e49cf284d667b1ad4f04d334d3c53a2cde22178f
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-11-21 18:03:41 +0530

    lambo

---
+++ global functions.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Lenovo on 21-11-2019.
+//
+
+#include <iostream>
+using namespace std;
+
+int a = 18;
+int b = 6;
+
+int function1(int a, int b) {
+    return a - b;
+}
+
+int function2() {
+    int c;
+    c = a + b;
+    return c;
+}
+
+int main() {
+    int b = 12;
+    int c = 0;
+    a = function1(b, a);
+    c = function2();
+    cout << "a: " << a << " b: " << b << " c: " << c << endl;
+}
